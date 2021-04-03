15cd3308d8110330b08995a9adfbb94134e5d038
Phil Lee <prodigy7748g@gmail.com>
2021-04-03 23:11:01 +0800

    add tests for creating session successfully

--- spec/controllers/access_tokens_controller_spec.rb
+++ spec/controllers/access_tokens_controller_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe AccessTokensController, type: :controller do
 
       it 'should return proper error body' do
         subject
-        expect(json['errors']).to include(error)
+        expect(json[:errors]).to include(error.deep_symbolize_keys)
       end
     end
 
@@ -39,11 +39,38 @@ RSpec.describe AccessTokensController, type: :controller do
       end
 
       subject { post :create, params: { code: 'invalid_code' } }
+
       it_behaves_like "unauthorized_requests"
     end
 
     context 'when success request' do
+      let(:user_data) do {
+        login: "phillee",
+        url: "http://example.com",
+        avatar_url: "http://example.com/avatar",
+        name: "Phil Lee"
+        }
+      end
 
+      before do
+        allow_any_instance_of(Octokit::Client).to receive(:exchange_code_for_token).and_return("valid_access_token")
+        allow_any_instance_of(Octokit::Client).to receive(:user).and_return(user_data)
+      end
+
+      subject { post :create, params: { code: "valid_code" }}
+
+      it "should return 201 status" do
+        subject
+        expect(response).to have_http_status(:created)
+      end
+
+      it "should return right json format" do
+        expect{ subject }.to change{ User.count }.by(1)
+        user = User.find_by(login: "phillee")
+        expect(json_data[:attributes]).to eq(
+          { "token" => user.access_token.token }.deep_symbolize_keys
+        )
+      end
     end
   end
 end
